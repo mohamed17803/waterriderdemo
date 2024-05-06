a67e08b428c350031f0be8390ae30ccbb6e25616
mohamed17803 <your_email@example.com>
2024-05-06 07:37:19 +0300

    Check MS 17!!

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -98,7 +98,7 @@ class LoginScreen extends StatelessWidget {
                       child: const Text(
                         'Sign Up',
                         style: TextStyle(
-                          fontSize: 43.0,
+                          fontSize: 42.0,
                           color: Colors.white,
                           decoration: TextDecoration.underline,
                           decorationThickness: 2,
